4ad456aa3562ebcf29d75ab5eb2451eb7f750a86
Yaroslav Mavliutov <yaroslav.mavliutov@gmail.com>
2018-10-13 14:29:07 +0200

    update

--- ransac.py
+++ ransac.py
@@ -1,6 +1,7 @@
 import numpy as np
 from harris_toy import main as HT
 from matplotlib import pyplot as plt
+import math
 
 def least_squares(xy1, xy2):
     """
@@ -10,19 +11,27 @@ def least_squares(xy1, xy2):
     try:
         m = (xy2[1] - xy1[1])/(xy2[0] - xy1[0])
         c = (xy1[1]*xy1[0] - xy1[0]*xy2[1])/(xy2[0] - xy1[0]) + xy1[1]
+        if m == float('nan') or c == float('nan'): return [0, 0]
         return [m, c]
+
     except: return [0, 0]
 
 def evaluate_model(X, y, m_c, inlier_threshold):
-
     count = 0
-    for i in range(0, len(X)):
-        if (y[i] - m_c[0]*X[i] + m_c[1]) <= inlier_threshold:
+    for i in range(0, len(X) - 1):
+        distance = abs(m_c[0]*X[i] - y[i] + m_c[1])/math.sqrt(m_c[0]**2 + 1)
+        # print('x ', X[i])
+        # print('y ', y[i])
+        # print('m_c ', m_c)
+        # print('d ', distance)
+        if distance <= inlier_threshold:
             count = count + 1
+        print(count)
+        print('--------------')
     return count
 
 
-def ransac(X, y, max_iters=1000, inlier_threshold=5, min_inliers=50):
+def ransac(X, y, max_iters=1000, inlier_threshold=20, min_inliers=16):
     best_model = None
     best_model_performance = 0
     #min_inliers = X.shape[0] - 1
@@ -57,6 +66,7 @@ model = ransac(np.asarray(xys[:, 1]), np.asarray(xys[:, 0]))
 
 y = []
 x = []
+print(model)
 for i in range(0, 300):
     if abs(i*model[0] + model[1])<= np.amax(np.asarray(xys[:, 0])):
         y.append(i*model[0] + model[1])
